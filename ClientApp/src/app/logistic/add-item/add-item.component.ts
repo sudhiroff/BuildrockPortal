17fbfd1a5d0d3685a02c4ded2a1a2a6d78bb4115
Sudhir Kumar <sudhiroff@gmail.com>
2021-03-06 14:52:12 +0530

    material upadte

--- ClientApp/src/app/logistic/add-item/add-item.component.ts
+++ ClientApp/src/app/logistic/add-item/add-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SalesService } from 'src/app/sales/sales.service';
 import { SharedService } from 'src/app/shared/shared.service';
